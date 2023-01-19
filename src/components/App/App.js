bebcda181488d486cfa36dc138ef76d644792d19
UliaKarpova <chelovek-e-95@mail.ru>
2023-01-19 12:04:33 +0300

    feat

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <section className="start-screen">
+      <div className='start-screen__logo'></div>
+      <p className='start-screen__text'>Первый сервис для поиска парковок</p>      
+      <button type='button' className='start-screen__button'>Искать парковки</button>
+    </section>
+  );
+}
+
+export default App;
